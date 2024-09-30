dd833d7b844f9f3aeb11e654e26397b90210aff7
Diamilatou-I <diamilatou.diallo@epitech.digital>
2024-09-30 16:02:26 +0200

    Create ex_04.js

---
+++ ex_04.js
@@ -0,0 +1,13 @@
+let str = "undefined"
+displayThisText(typeof undefined)
+let boolean = true
+displayThisText (typeof true)
+let number = 4
+displayThisText(typeof 4)
+let bigint = 4n
+displayThisText(typeof 4n)
+let symbol = Symbol ()
+displayThisText(typeof Symbol ())
+let object = []
+displayThisText(typeof [])
+displayThisText(typeof function dede(){})
